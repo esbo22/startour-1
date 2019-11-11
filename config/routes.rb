d391e79e0f8d995b922a3ca1de578b5803db339c
doug delpha <douglasdelpha@gmail.com>
2019-11-11 17:19:22 +0100

    completed adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :ships do
+    resources :bookings, only: [:create]
 end
+resources :bookings, only: [:index, :show, :destroy, :update] do
+  resources :ships, only: [:create, :index, :show]
+ end
+
+namespace :owner do
+  resources :ships, only: [:index]
+  resources :bookings, only: [:index, :update]
+ end
+
+end
+
