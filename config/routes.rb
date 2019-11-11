fe98d78d4fefa8c25080436749c856e1c0cb0db3
doug delpha <douglasdelpha@gmail.com>
2019-11-11 17:40:31 +0100

    corrected routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,22 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :ships do
-    resources :bookings, only: [:create]
-end
-resources :bookings, only: [:index, :show, :destroy, :update] do
-  resources :ships, only: [:create, :index, :show]
- end
 
-namespace :owner do
-  resources :ships, only: [:index]
-  resources :bookings, only: [:index, :update]
- end
+  resources :ships, only: [:new, :create, :index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index]
+
+  namespace :owner do
+    resources :ships, only: [:index]
+    resources :bookings, only: [:index] do
+      member do
+        patch "accept"
+        patch "decline"
+      end
+    end
+  end
 
 end
 
