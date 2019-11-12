977e0f6138f24df5b54633d4d920663b00123304
doug delpha <douglasdelpha@gmail.com>
2019-11-12 14:47:41 +0100

    finished ship view,controller

---
+++ app/controllers/ships_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class ShipsController < ApplicationController
+  before_action :set_ship, only: [:show]
+  skip_before_action :authenticate_user!, only: %i[new index]
+
+  def index
+    @ships = Ship.all
+  end
+
+  def show
+    @booking = Booking.new
+    @ship = Ship.find(params[:id])
+    @bookings = @ship.bookings
+    @alert_message = "You are viewing the ship of #{@ship.owner.first_name}"
+  end
+
+  def new
+    @ship = Ship.new
+  end
+
+  def create
+    @ship = Ship.new(ship_params)
+    @ship.user = current_user
+    if @ship.save
+      redirect_to_ship_path(@ship)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ship_params
+    params.require(:ship).permit(:address, :model, :photo, :price_per_day, :description, :photo, :captain, :capacity_max, :breakfast)
+  end
+
+  def set_ship
+    @ship = Ship.find(params[:id])
+  end
+end
