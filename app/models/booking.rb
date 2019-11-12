8b8e34ee8ab0a8cde806b54e5e3e973212578034
Yann Ropert <yann.ropert@gmail.com>
2019-11-12 11:53:28 +0100

    Seeds problem solved

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :ship
   belongs_to :user
 
-  validates :status, presence: true, inclusion: { in: ["Pending user request", "Pending owner validation", "Confirmed", "Canceled"], allow_nil: false }
+  validates :status, presence: true, inclusion: { in: ["pending", "confirmed", "canceled"], allow_nil: false }
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :total_price, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,36 +8,40 @@ require 'open-uri'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroy all instances"
+Booking.destroy_all if Rails.env.development?
 Ship.destroy_all if Rails.env.development?
 User.destroy_all if Rails.env.development?
 
 #DAPHNEE
 daph = User.new(email: "daph@example.com", password: "password", first_name: "Daphnée", last_name: "Duportal")
-daph.save
+daph.save!
+
 pod = Ship.new(model: "Pod-Racer", price_per_day: 60, capacity_max: 1, address: "Chez Daphnée, Caen")
 pod.owner = daph
-pod.save
+pod.save!
 
 #JOHNNY
 johnny = User.new(email: "johnny@example.com", password: "password", first_name: "Johnny", last_name: "Girault")
-johnny.save
+johnny.save!
+
 x_wing_2 = Ship.new(model: "X-Wing", capacity_max: 1, description: "Rebellion starfighter", price_per_day: 100, captain: true, address: "Lannion, France")
 x_wing_2.owner = johnny
-x_wing_2.save
+x_wing_2.save!
 
 #YANN
 yann = User.new(email: "yann@example.com", password: "password", first_name: "Yann", last_name: "Ropert")
-yann.save
+yann.save!
+
 tie_bomb = Ship.new(model: "Tie Bomber", capacity_max: 1, description: "Bombing variant of the TIE line used by the Galactic Empire", price_per_day: 120, captain: true, address: "Brest, France")
 tie_bomb.owner = yann
-tie_bomb.save
+tie_bomb.save!
 
 
-status = ["Pending user request", "Pending owner validation", "Confirmed", "Canceled"]
+status = ["pending", "confirmed", "canceled"]
 url_ship = "https://res.cloudinary.com/yannr/image/upload/v1573553018/x-wing_rrx1sl.png"
 url_avatar = "https://res.cloudinary.com/yannr/image/upload/v1573553210/darth_vader_mjxmvx.png"
-puts "Destroy all instances"
-Booking.destroy_all if Rails.env.development?
+
 
 puts "Create users"
 20.times do
@@ -49,27 +53,35 @@ puts "Create users"
    bio: Faker::Lorem.paragraph,
    )
  user.remote_avatar_url = url_avatar
- user.save
+ user.save!
 end
+
+puts "Create ships"
 20.times do
  ship = Ship.new(
    model: Faker::Vehicle.model,
-   capacity_max: 1..5,
+   capacity_max: rand(1..5),
    address: Faker::Address.street_address,
    description: Faker::Lorem.paragraph,
+   captain: true,
    price_per_day: Faker::Number.between(from: 20, to: 200),
    owner: User.all.sample
    )
  ship.remote_photo_url = url_ship
- ship.save
+ ship.save!
 end
+
+puts "Create bookings"
 20.times do
- Booking.create(
+ Booking.create!(
    start_date: Date.today,
    end_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
    status: status.sample,
    total_price: Faker::Number.between(from: 20, to: 200),
    ship: Ship.all.sample,
-   number_of_guests: User.all.sample
+   number_of_guests: rand(1..5),
+   user: User.all.sample
    )
 end
+
+puts "Done !"
