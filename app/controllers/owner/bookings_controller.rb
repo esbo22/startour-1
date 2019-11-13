d94bcd4ef67e3f01e3498b98631b4d23e0d8c4a2
doug delpha <douglasdelpha@gmail.com>
2019-11-13 14:00:05 +0100

    started owner controller and view

---
+++ app/controllers/owner/bookings_controller.rb
@@ -0,0 +1,29 @@
+class Owner::BookingsController < ApplicationController
+  def index
+    @ships = current_user.ships
+  end
+
+  def accept
+    @booking = Booking.new
+    @booking.status = "confirmed"
+    @booking.save
+    redirect_to owner_ships_path
+  end
+
+  def decline
+    @booking = Booking.new
+    @booking.status = "canceled"
+    @booking.save
+    redirect_to owner_ships_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  # def booking_params
+  #   params.require(:booking).permit(:start_date, :end_date, :number_of_guests, :captain)
+  # end
+end
