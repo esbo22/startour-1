10e295733c3b82b768a094bfe086a4b3a7f6df5f
Yann Ropert <yann.ropert@gmail.com>
2019-11-14 17:24:22 +0100

    edit action, route and view

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ShipsController < ApplicationController
-  before_action :set_ship, only: [:show]
+  before_action :set_ship, only: [:show, :edit]
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
@@ -32,6 +32,9 @@ class ShipsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
   end
 
   private
