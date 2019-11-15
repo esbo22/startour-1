a245f90d878791cb17f7e1a93c6dcc2a33b53619
Yann Ropert <yann.ropert@gmail.com>
2019-11-15 14:50:42 +0100

    ship model without captain boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_104838) do
+ActiveRecord::Schema.define(version: 2019_11_15_134830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2019_11_14_104838) do
     t.integer "capacity_max"
     t.text "description"
     t.float "price_per_day"
-    t.boolean "captain"
     t.string "photo"
     t.string "address"
     t.datetime "created_at", null: false
